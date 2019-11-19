cf188b5d223b7d2f62c7f08a079c0c14b0e5c3ae
Natalalia <n.olmo.megias@gmail.com>
2019-11-19 21:18:03 +0000

    comparison of selected char showing either correct letter or fail step

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,26 +6,37 @@ import Hangman from "./Hangman";
 
 class Game extends React.Component {
   state = {
-    hiddenSentence: "_ _ _ _ _ _ _",
-    selectedChar: "",
-    sentenceToGuess: "Titanic",
+    hiddenSentence: ["_", "_", "_", "_", "_", "_", "_"],
+    sentenceToGuess: "TITANIC",
     hangmanSteps: 0
   };
   render() {
     const { hiddenSentence, hangmanSteps } = this.state;
+
     return (
       <>
-        <Chars charSelector={this.charSelector} />
+        <Chars charSelector={this.onCharSelected} />
         <HiddenSentence hiddenSentence={hiddenSentence} />
         <Hangman hangmanSteps={hangmanSteps} />
       </>
     );
   }
 
-  charSelector = char => {
-    this.setState({
-      selectedChar: char
-    });
+  onCharSelected = char => {
+    const { sentenceToGuess } = this.state;
+    let { hiddenSentence, hangmanSteps } = this.state;
+    let anyExist = false;
+
+    for (let i = 0; i < sentenceToGuess.length; i++) {
+      if (char === sentenceToGuess[i]) {
+        anyExist = true;
+        hiddenSentence.splice(i, 1, char);
+      }
+    }
+    if (!anyExist) {
+      hangmanSteps++;
+    }
+    this.setState({ hiddenSentence, hangmanSteps });
   };
 }
 
